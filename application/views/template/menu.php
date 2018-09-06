fcb2bf752b8bd9eab379717fdbbb4844b589b71d
agungdh <agungdh@live.com>
2018-09-07 01:09:00 +0700

    delete config

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -13,7 +13,3 @@
 <?php if ($this->session->level == 'a') { ?>
 <li><a class="app-menu__item" href="<?php echo base_url('user'); ?>"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">User</span></a></li>
 <?php } ?>
-
-<?php if ($this->session->level == 'a') { ?>
-<li><a class="app-menu__item" href="<?php echo base_url('config'); ?>"><i class="app-menu__icon fa fa-gears"></i><span class="app-menu__label">Config</span></a></li>
-<?php } ?>
